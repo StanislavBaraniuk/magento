9f2f7085a9923b3f20397132c83f27299dac9b0a
Stanislav <stanislavbaraniuk@gmail.com>
2019-02-21 18:10:23 +0200

    Update IndexController.php

--- app/code/local/Baraniuk/Newproducts/controllers/IndexController.php
+++ app/code/local/Baraniuk/Newproducts/controllers/IndexController.php
@@ -2,11 +2,6 @@
 
 class Baraniuk_Newproducts_IndexController extends Mage_Core_Controller_Front_Action {
     public function listAction () {
-//        Mage::dispatchEvent('page_block_html_topmenu_gethtml_after', array(
-//            'menu' => new Varien_Data_Tree_Node(array(), 'root', new Varien_Data_Tree()),
-//            'html' => '<div>asdasd</div>'
-//        ));
-
         $this->loadLayout();
         $this->renderLayout();
     }
