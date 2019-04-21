4dd1618741343d168fcc5db1d83ad3fd7c3413de
stanislaw <stanislavbaraniuk@gmail.com>
2019-04-21 03:44:20 +0300

    Magento 1

--- app/code/local/Baraniuk/IAI/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    class Baraniuk_IAI_Helper_Data extends Mage_Core_Helper_Abstract
-    {
-
-    }
---
+++ ddd.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: stanislaw
+     * Date: 2/13/19
+     * Time: 22:08
+     */
+
+    phpinfo() ;
